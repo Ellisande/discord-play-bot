6e97c37ed53a2320cffaa0ebdc264aed4f901986
Ellisande <justin.dragos@gmail.com>
2018-06-13 22:57:15 -0700

    Add lodash to clean things up a little.

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.io");
 const winston = require("winston");
 const express = require("express");
 const axios = require("axios");
+const _ = require("lodash");
 const auth = process.env.NODE_ENV == "production" ? {} : require("./auth.json");
 
 const logger = winston.createLogger({
@@ -33,12 +34,9 @@ const channels = {};
 
 const gamesByChannel = {};
 
-const addPlayer = (game, playerName, channelID) => {
-  if (!channels[channelID]) {
-    logger.info(`Initializing the channel container: ${channelID}`);
-    channels[channelID] = {};
-  }
-  const games = channels[channelID];
+const addPlayer = (game, playerName, channelId) => {
+  _.merge(channels[channelId], {});
+  const games = channels[channelId];
   if (!games[game]) {
     logger.info(`Initalizing the game: ${game}`);
     games[game] = [];
@@ -52,12 +50,10 @@ const addPlayer = (game, playerName, channelID) => {
   games[game] = [...games[game], playerName];
 };
 
-const getPlayerList = (game, channelId) => {
-  return channels[channelId] ? channels[channelId][game] : [] || [];
-};
+const getPlayerList = (game, channelId) =>
+  _.get(channels, `${channelId}.${game}`, []);
 
-const getAllGames = channelId =>
-  channels[channelId] ? Object.keys(channels[channelId]) : [];
+const getAllGames = channelId => Object.keys(_.get(channels, channelId, {}));
 
 bot.on("ready", event => {
   logger.info("Connected");
@@ -66,14 +62,15 @@ bot.on("ready", event => {
 });
 
 bot.on("message", (user, userID, channelID, message, event) => {
-  logger.info("Got message!");
-  logger.info(message);
+  logger.info(`Got message from ${userID}!`);
+
   if (!message.match(/^!/)) {
     logger.info("Message didn't match /^!/");
     return;
   }
 
   const command = message.split(" ")[0];
+
   logger.info(`Command received: ${command}`);
   const game = message.replace(RegExp(`^${command} `), "").toLowerCase();
 
@@ -88,10 +85,9 @@ bot.on("message", (user, userID, channelID, message, event) => {
     case "!who_plays":
       const players = getPlayerList(game, channelID);
       const playerMentions = players.map(userId => `<@${userId}>`).join(", ");
-      const message =
-        players.length > 0
-          ? `${game} players: ${playerMentions}`
-          : `No on plays ${game} or they just don't want to admit it`;
+      const message = !is_empty(playerMentions)
+        ? `${game} players: ${playerMentions}`
+        : `No on plays ${game} or they just don't want to admit it`;
       bot.sendMessage({
         to: channelID,
         message: message
@@ -116,5 +112,6 @@ bot.on("message", (user, userID, channelID, message, event) => {
         to: channelID,
         message: "!i_play, !who_plays, !games"
       });
+      break;
   }
 });
