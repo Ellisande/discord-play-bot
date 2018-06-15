677944ffb912486c5dea4cc7e317319d86011bea
Ellisande <justin.dragos@gmail.com>
2018-06-14 22:44:42 -0700

    Second take on building playbot!

--- bot.js
+++ bot.js
@@ -1,51 +1,30 @@
 const Discord = require("discord.io");
 const winston = require("winston");
-const express = require("express");
-const axios = require("axios");
 const _ = require("lodash");
-const auth = process.env.NODE_ENV == "production" ? {} : require("./auth.json");
+const admin = require("firebase-admin");
+
+const discordAuth =
+  process.env.NODE_ENV == "production" ? {} : require("./auth.json");
+
+const firebaseCert = process.env.NODE_ENV
+  ? proccess.env.firebaseCert
+  : require("./firebaseCert.json");
 
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console({ colorize: true })]
+  transports: [new winston.transports.Console()]
 });
 logger.level = "debug";
 
-const app = express();
-
-app.get("/ping", (req, res) => res.send("Pong!"));
-
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Running web server for... no reason?")
-);
-
-const pingUrl =
-  process.env.NODE_ENV == "production"
-    ? "https://discord-play-bot.herokuapp.com/ping"
-    : "http://localhost:3000/ping";
-
-setInterval(() => axios.get(pingUrl), 10000);
-
-const bot = new Discord.Client({
-  token: process.env.bot_token || auth.token,
-  autorun: true
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseCert)
 });
 
-const channels = {};
+const db = admin.firestore();
 
-const gamesByChannel = {};
-
-const addPlayer = (game, playerId, channelId) => {
-  const playerList = _.get(channels, `${channelId}.${game}`, []);
-  if (playerList.includes(playerId)) {
-    return playerList;
-  }
-  _.set(channels, `${channelId}.${game}`, [...playerList, playerId]);
-};
-
-const getPlayerList = (game, channelId) =>
-  _.get(channels, `${channelId}.${game}`, []);
-
-const getAllGames = channelId => Object.keys(_.get(channels, channelId, {}));
+const bot = new Discord.Client({
+  token: process.env.bot_token || discordAuth.token,
+  autorun: true
+});
 
 bot.on("ready", event => {
   logger.info("Connected");
@@ -53,57 +32,72 @@ bot.on("ready", event => {
   logger.info(`${bot.username} - (${bot.id})`);
 });
 
-bot.on("message", (user, userID, channelID, message, event) => {
-  logger.info(`Got message from ${userID}!`);
+const toMentions = playerId => `<@${playerId}>`;
 
-  if (!message.match(/^!/)) {
-    logger.info("Message didn't match /^!/");
+const docOrDefault = defaultValue => doc => (doc.exists ? doc.data() : []);
+
+const getPlayers = gameName => games => games[gameName];
+
+const buildWhoPlaysMessage = (players, gameName) =>
+  `${gameName} players: ${players.map(toMentions).join(", ")}`;
+
+const sendMessage = (channelId, message) =>
+  bot.sendMessage({
+    to: channelId,
+    message
+  });
+
+bot.on("message", (user, userID, channelID, message, event) => {
+  logger.info(`Got message from ${userID} in channel ${channelID}!`);
+
+  if (channelID != "457011209372303371") {
+    logger.info("Not for the test channel");
     return;
   }
 
   const command = message.split(" ")[0];
+  const gameName = message.replace(RegExp(`^${command} `), "");
 
-  logger.info(`Command received: ${command}`);
-  const game = message.replace(RegExp(`^${command} `), "").toLowerCase();
+  logger.info(`[${command}] with game ${gameName} from ${userID}`);
 
-  switch (command) {
-    case "!ping":
-      bot.sendMessage({
-        to: channelID,
-        message: "Pong!"
+  if (command == "!i_play_test") {
+    const gameDoc = db.doc(`/channels/${channelID}/games/${gameName}`);
+
+    db.runTransaction(t => {
+      return t.get(gameDoc).then(doc => {
+        const oldPlayers = doc.data().players;
+        if (oldPlayers.includes(userID)) {
+          logger.info(
+            `[${command}] player ${userID} already plays ${gameName}`
+          );
+          throw "Player already plays";
+        }
+        const newPlayers = [...oldPlayers, userID];
+        t.update(gameDoc, { players: newPlayers });
       });
-      logger.info("I pinged!");
-      break;
-    case "!who_plays":
-      const players = getPlayerList(game, channelID);
-      const playerMentions = players.map(userId => `<@${userId}>`).join(", ");
-      const message = !_.isEmpty(playerMentions)
-        ? `${game} players: ${playerMentions}`
-        : `No on plays ${game} or they just don't want to admit it`;
-      bot.sendMessage({
-        to: channelID,
-        message: message
+    })
+      .then(() => {
+        logger.info(`[${command}] players ${userID} now plays ${gameName}`);
+        bot.sendMessage({
+          to: channelID,
+          message: `You are now playing ${gameName}`
+        });
+      })
+      .catch(() => {
+        bot.sendMessage({
+          to: channelID,
+          message: `<@${userID}> already play ${gameName}`
+        });
       });
-      break;
-    case "!i_play":
-      addPlayer(game, userID, channelID);
-      bot.sendMessage({
-        to: channelID,
-        message: `${user} now plays ${game}`
-      });
-      break;
-    case "!games":
-      const allGames = getAllGames(channelID);
-      bot.sendMessage({
-        to: channelID,
-        message: allGames.join(", ")
-      });
-      break;
-    case "!commands":
-      bot.sendMessage({
-        to: channelID,
-        message: "!i_play, !who_plays, !games"
-      });
-      break;
+  }
+
+  if (command == "!who_plays") {
+    const gameDoc = db.doc(`/channels/${channelID}/games/${gameName}`);
+    gameDoc
+      .get()
+      .then(docOrDefault([]))
+      .then(game => game.players)
+      .then(players => buildWhoPlaysMessage(players, gameName))
+      .then(message => sendMessage(channelID, message));
   }
 });
